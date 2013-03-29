4dc44995120d5114dd4899985a2c2fe623bcaf7b
halversotm30 <halversotm30@uww.edu>
2013-03-29 17:36:20 -0500

    Update index.php
    Just took out the php snippet that called for session_destroy(). 
    
    It was executing every time the page was loaded instead of when the exit button was clicked.

--- MainMenu/index.php
+++ MainMenu/index.php
@@ -3,9 +3,13 @@ session_start();
 
 include_once'../../Subway/HelperFiles/config.php';
 include_once'../../Subway/HelperFiles/employeeClass.php';
+include_once'../../Subway/HelperFiles/unsetEmpFields.php';
+
+if (isset($_SESSION['no_product']))
+    unset($_SESSION['no_product']);
 
 
-$sqlCommand = "SELECT employee_id, first_name, last_name from subway.employee";
+$sqlCommand = "SELECT idemp, first_name, last_name from test.emp";
 
 $result = mysqli_query($db_connect, $sqlCommand);
 
@@ -17,14 +21,13 @@ while ($row = mysqli_fetch_array($result)) {
 
     $employee = new employeeClass;
 
-    $employee->setEmployeeID($row["employee_id"]);
+    $employee->setEmployeeID($row["idemp"]);
     $employee->setEmployeeFirstName($row["first_name"]);
     $employee->setEmployeeLastName($row["last_name"]);
     array_push($schedule_array, $employee);
 }
 
-$_SESSION['schedule_array']=$schedule_array;
-
+$_SESSION['schedule_array'] = $schedule_array;
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,81 +38,71 @@ $_SESSION['schedule_array']=$schedule_array;
     </head>
     <body>
 
-            <div id="page_top">
-                <div id="top_image">
-                
-                    <img src="/Images/temp_top_logo.png" align="center">
-                </div>
-            
+        <div id="page_top">
+            <div id="top_image">
+                <img src="/Images/temp_top_logo_3.png" align="center">
+            </div>
+
             <ul class="subway_tabs">
-                <li class="current_position">Welcome:</li>
+                <li class="current_position">Home:</li>
                 <li><a href="/ManageSchedule/index.php">Create Schedule</a></li>
                 <li><a href="/ViewSchedule/index.php">View Schedule:</a></li>
                 <li><a href="/ManageEmployee/index.php">Employees:</a></li>
                 <li><a href="/EditRequests/index.php" >Requests:</a></li>
                 <li><a href="/ScheduleParameters/index.php">Business Rules:</a></li>
             </ul>       
-            
+
             <div id="tab_bar">
-            
+
             </div>
 
-        <div id="welcome_body">
-        
-        
-            <div id="welcome_left">
-                
-                <form action="/HelperFiles/changePassword.php" method="POST">
-                    
-                    <input type="submit" value ="Change Password"> 
-                </form>
-                 </br>
+            <div id="welcome_body">
+                <div id="welcome_left">
+
+                    <form action="/HelperFiles/changePassword.php" >
+                        <input type="submit" value ="Change Password"> 
+                    </form>
                     </br>
-                <form action="<?php session_destroy();?>/index.php" method="POST">
-                    <input type="submit" value="Exit">
-                </form>
-                
+                    </br>
+                    <form action="/index.php" method="POST">
+                        <input type="hidden" id="exit" name="exit" value="destroy">
+                        <input type="submit" value="Exit">
+                    </form>
+
+                </div>
+
+                <div id="welcome_right">
+
+                    <div id="welcome_title">This Week's Schedule</div>
+                    <div id="welcome_sched">
+                        <table>
+                            <tr><th>Employee:</th><th>Wednesday:</th><th>Thursday:</th><th>Friday:</th><th>Saturday:</th>
+                                <th>Sunday:</th><th>Monday:</th><th>Tuesday:</th><th>Total Hours:</th></tr> 
+
+                            </br>   
+
+                            <?php
+                            for ($x = 0; $x < count($_SESSION['schedule_array']); $x++) {
+
+                                echo "<tr><td class='sched_emp'>";
+                                echo $_SESSION['schedule_array'][$x]->getEmployeeFirstName() . " ";
+                                echo $_SESSION['schedule_array'][$x]->getEmployeeLastName() . " </td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "<td><p class='generate_text_field'></p></td>";
+                                echo "</tr>";
+                            }
+                            ?>
+                        </table>
+                    </div>    
+                </div>
             </div>
-            
-            <div id="welcome_right">
-                
-                <div id="welcome_title">This Week's Schedule</div>
-                <div id="welcome_sched">
-                    <table>
-                    <tr><th>Employee:</th><th>Wednesday:</th><th>Thursday:</th><th>Friday:</th><th>Saturday:</th>
-                    <th>Sunday:</th><th>Monday:</th><th>Tuesday:</th><th>Total Hours:</th></tr> 
-                
-                    </br>   
-                  
-                <?php
-                
-                    for($x=0;$x<count($_SESSION['schedule_array']);$x++){
-                        
-                        echo "<tr><td class='sched_emp'>";
-                        echo $_SESSION['schedule_array'][$x]->getEmployeeFirstName()." ";
-                        echo $_SESSION['schedule_array'][$x]->getEmployeeLastName()." </td>";
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "<td><p class='generate_text_field'></p></td>";    
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "<td><p class='generate_text_field'></p></td>";
-                        echo "</tr>";
-                    }
-                ?>
-                </table>
-                </div>    
-                    
-                
-                
-            </div>
-            
-            
         </div>
-        </div>
-        
     </body>
 </html>
 
