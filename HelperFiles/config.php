cb680d70c1e700e795dc37e7aea521a547ab3692
Rob Hefty <heftyra12@uww.edu>
2013-04-09 21:43:01 -0500

    Update db name in config file
    I'm not even sure why this was working as it was before, but to be
    consistent I altered the dbname from "test" to "subway."

--- HelperFiles/config.php
+++ HelperFiles/config.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "admin";
-$db_name = "test";
+$db_name = "subway";
 
 $db_connect =mysqli_connect($db_host,$db_user,$db_pass,$db_name) or DIE("Unable to connect to the database:");
 
