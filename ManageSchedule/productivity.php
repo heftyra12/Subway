6abc2002f21450589a3dd610866aa466a9fa7d3f
halversotm30 <halversotm30@uww.edu>
2013-04-15 12:48:58 -0500

    productivity form reset and only displays current or > productivity

--- ManageSchedule/productivity.php
+++ ManageSchedule/productivity.php
@@ -4,9 +4,6 @@ include_once'../HelperFiles/config.php';
 include_once'../HelperFiles/productivityClass.php';
 include_once'../../Subway/HelperFiles/unsetEmpFields.php';
 
-if (isset($_SESSION['no_product']))
-    unset($_SESSION['no_product']);
-
 $prodSQLCommand = 'SELECT store_id, week_no, units FROM subway.productivity';
 $result = mysqli_query($db_connect,$prodSQLCommand);
 
@@ -35,13 +32,11 @@ while($row = mysqli_fetch_array($result)){
 }
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
     <head>
-
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel='stylesheet' href='/CSS/subway_css.css' type='text/css'>
-
         <title></title>
     </head>
     <body>
@@ -108,7 +103,7 @@ while($row = mysqli_fetch_array($result)){
                                                        $newest_week = $prod_array[$x]->getWeekNumber();
                                                    }                    
                                                }
-                  
+                                               
                                                $week_values = $newest_week;
                                                for($x=0;$x < 4; $x++){                                 
                                                    $week_values++;
@@ -125,7 +120,7 @@ while($row = mysqli_fetch_array($result)){
                                         echo"</select>";
                                         echo "</td>";
                                         ?>
-                                                          
+                                                         
                                 <td><input type ="text" name="wed_prod" id="wed_prod" class="product_field" required></td>
                                 <td><input type="text" name="thurs_prod" id="thurs_prod" class="product_field" required></td>
                                 <td><input type="text" name="fri_prod" id="fri_prod" class="product_field" required></td>
@@ -134,17 +129,20 @@ while($row = mysqli_fetch_array($result)){
                                 <td><input type="text" name="mon_prod" id="mon_prod" class="product_field" required></td>
                                 <td><input type="text" name="tues_prod" id="tues_prod" class ="product_field" required></td>
                             </tr>
-
-
-                            <input type="hidden" id="productivity" name="productivity" value=""/>
-                            <input type="hidden" id="generate" name="generate" value=""/>
-                            <input type="hidden" id="edit" name="edit" value=""/>
+                             
+                                <input type='hidden' name='newest_week' id='newest_week' value='<?php echo $newest_week;?>'>;
+                                <input type="hidden" id="productivity" name="productivity" value=""/>
+                                <input type="hidden" id="generate" name="generate" value=""/>
+                                <input type="hidden" id="edit" name="edit" value=""/>
+                                <input type="hidden" id="current_week" name="current_week" value="<?php echo date("W");?>">
+                                <input type="hidden" id="current_prod" name="current_prod" value="<?php echo $_SESSION['current_prod'];?>">
+                            
                             <tr>
                                 <th colspan="7" style="text-align:right;">Action Choice:</th>
                                 <td><select id="update_choice" name="update_choice" onChange ="weekUpdate();">
-                                        
                                     </select></td>
                             </tr>
+                            <tr><td colspan="8"><input type="button" value="Reset" onclick="resetForm();"></td></tr>
                             <tr>
                                 <td colspan="8">
                                     <input type="submit" value="Submit Productivity"/>
@@ -184,27 +182,30 @@ while($row = mysqli_fetch_array($result)){
                                     $mon_prod = $prod_array[$x]->getMonProd();
                                     $tues_prod = $prod_array[$x]->getTuesProd();
                                     
-                                    echo "<tr><td>";
-                                    echo "<input type='radio' name ='prod' id='prod' value ='' 
-                                            onclick='insertProd($x,\"$week_number\",\"$wed_prod\",\"$thurs_prod\",
-                                            \"$fri_prod\",\"$sat_prod\",\"$sun_prod\",\"$mon_prod\",\"$tues_prod\");'></td>";
-                                    echo "<td>";
-                                    echo $week_number;
-                                    echo "</td><td>";
-                                    echo $wed_prod;
-                                    echo "</td><td>";
-                                    echo $thurs_prod;
-                                    echo "</td><td>";
-                                    echo $fri_prod;
-                                    echo "</td><td>";
-                                    echo $sat_prod;
-                                    echo "</td><td>";
-                                    echo $sun_prod;
-                                    echo "</td><td>";
-                                    echo $mon_prod;
-                                    echo "</td><td>";
-                                    echo $tues_prod;
-                                    echo "</td></tr>";
+                                    if($prod_array[$x]->getWeekNumber() >= date("W")){
+                                 
+                                        echo "<tr><td>";
+                                        echo "<input type='radio' name ='prod' id='prod' value ='' 
+                                                onclick='insertProd($x,\"$week_number\",\"$wed_prod\",\"$thurs_prod\",
+                                                \"$fri_prod\",\"$sat_prod\",\"$sun_prod\",\"$mon_prod\",\"$tues_prod\");'></td>";
+                                        echo "<td>";
+                                        echo $week_number;
+                                        echo "</td><td>";
+                                        echo $wed_prod;
+                                        echo "</td><td>";
+                                        echo $thurs_prod;
+                                        echo "</td><td>";
+                                        echo $fri_prod;
+                                        echo "</td><td>";
+                                        echo $sat_prod;
+                                        echo "</td><td>";
+                                        echo $sun_prod;
+                                        echo "</td><td>";
+                                        echo $mon_prod;
+                                        echo "</td><td>";
+                                        echo $tues_prod;
+                                        echo "</td></tr>";
+                                    }
                                 }
                             ?>
                         </table>
@@ -220,18 +221,60 @@ while($row = mysqli_fetch_array($result)){
 
 <script language="Javascript">
     
+    function resetForm(){
+        
+        document.getElementById('wed_prod').value = "";
+        document.getElementById('thurs_prod').value = "";
+        document.getElementById('fri_prod').value = ""; 
+        document.getElementById('sat_prod').value = ""; 
+        document.getElementById('sun_prod').value = "";
+        document.getElementById('mon_prod').value = "";
+        document.getElementById('tues_prod').value = "";
+        document.getElementById('update_choice').options.length=0;
+        
+        var list = document.getElementsByName('prod');
+        
+        for(var x = 0; x < list.length; x++){
+            
+            if(list[x].checked == true)
+                list[x].checked = false; 
+        }
+        
+        var current_production = document.getElementById("current_prod").value;
+        var current_week = document.getElementById("current_week").value;
+        var newest_week = document.getElementById("newest_week").value;
+        var week_list = document.getElementById("week_list");
+        
+        if(current_production == true){
+            
+            week_list.options.length = 0; 
+            var option = document.createElement("Option");
+            option.text ="---";
+            week_list.options[0] = option; 
+            
+            for(var x = 1; x < 5; x++){
+                newest_week++;
+                var option = document.createElement("Option");
+                option.value = newest_week;
+                option.text = newest_week;
+                week_list.options[x] = option;
+            }
+        }
+        else{
+            
+            week_list.options.length = 0; 
+            var option = document.createElement("Option");
+            option.value = current_week;
+            option.text = current_week;
+            week_list.options[0]=option;
+        }     
+    }
+    
     function insertProd(index,week_number, wed_prod,thurs_prod,fri_prod,sat_prod,sun_prod,mon_prod,tues_prod){
 
         var index = index;
         var week_number = week_number;
-        var wed_prod = wed_prod; 
-        var thurs_prod = thurs_prod; 
-        var fri_prod = fri_prod;
-        var sat_prod = sat_prod;
-        var sun_prod = sun_prod;
-        var mon_prod = mon_prod; 
-        var tues_prod = tues_prod; 
-      
+        
         document.getElementById('wed_prod').value = wed_prod;
         document.getElementById('thurs_prod').value = thurs_prod;
         document.getElementById('fri_prod').value = fri_prod; 
