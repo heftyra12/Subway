210da333cde1fb75ac33c6453f8794bbce8c5abc
halversotm30 <halversotm30@uww.edu>
2013-05-09 15:06:29 -0500

    built in php filter string

--- HelperFiles/validateUser.php
+++ HelperFiles/validateUser.php
@@ -1,86 +1,38 @@
 <?php
-  session_start();
+    session_start();
 
     include_once'config.php';
  
-    $user_name = $_POST['user_name'];
-    $password = $_POST['password'];
-    
-    $dbName ="";
-    $dbPass ="";
-    
-    //Call the validateUser function.
-    $stripped_name = validateUser($user_name);
-    $stripped_password = validateUser($password);    
-     
-    function validateUser($user_info){
-    
-        //Variables to hold the user's login information
-        $temp_info = "";
-        $user_input = $user_info;
-        
-       //For loop to go through each character of the user's login information
-        for($x=0; $x<strlen($user_input);$x++){
-            
-            switch($user_info[$x]){
-                
-                case'(': $temp_info.="";
-                    break;
-                case')': $temp_info.="";
-                    break;
-                case'<': $temp_info.="";
-                    break;
-                case'>': $temp_info.="";
-                    break;
-                case';': $temp_info.="";
-                    break;
-                case'"': $temp_info.="";
-                    break;
-                case'&': $temp_info.="";
-                    break;
-                case'@': $temp_info.='';
-                    break;
-                default: $temp_info.=$user_info[$x];
-            }
-        }
-        return $temp_info;
-    }
-    
+    $user_name = filter_var($_POST['user_name'], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+   
     if (mysqli_connect_errno()) {
         echo "DB connect error: %s\n", mysqli_connect_error();  
     }
-    
-    
-    $loginSQL = 'SELECT user_name, password FROM subway.employee WHERE user_name="'.$stripped_name.'" and password="'.$stripped_password.'"';
+  
+    $loginSQL = 'SELECT user_name, password FROM subway.employee WHERE user_name="'. $user_name .'" and password="'.$password.'"';
     $result = mysqli_query($db_connect,$loginSQL);
 
     if (false === $result) 
         echo "DB fetch error: %s\n", mysqli_error($db_connect);
     
-    while($row = mysqli_fetch_array($result)){
-        $dbName = $row["user_name"];
-        $dbPass = $row["password"];
-    }
+   $row = mysqli_fetch_array($result);
 
-    /*$sqlCommand = "update test.employee set name=boyeah where employee_id=1";
-    $query = mysqli_query($con, $sqlCommand);*/
-    
-    if($stripped_name === $dbName && $stripped_password === $dbPass){
-        $_SESSION['user_name'] = $stripped_name;
-        $_SESSION['user_password'] = $stripped_password;
+    if($user_name === $row["user_name"] && $password === $row["password"])
+    {
+        $_SESSION['user_name'] = $user_name;
+        $_SESSION['user_password'] = $password;
         
         unset($_SESSION['user_name_error']);
         unset($_SESSION['password_error']);
             
         header("Location: /MainMenu/index.php");
     }
-    else{
-        
-        if($stripped_name !== $dbName || $stripped_password !== $dbPass){
-            $_SESSION['user_name_error'] = "Error: Invalid Log in ";
-            $_SESSION['password_error'] = "Error: Invalid Log in";
-        }
-          
+    else
+    {    
+        $_SESSION['user_name_error'] = "Error: Invalid Login";
+        $_SESSION['password_error'] = "Error: Invalid Login";
+    
         header("Location: /index.php");
     }
 ?>
